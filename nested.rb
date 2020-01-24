89c77b196a84572ed9ce501ea84c367a69b0ddda
Yuksalbek Rakhimjonov <yuksal97@gmail.com>
2020-01-24 06:38:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,8 +86,10 @@ def adding_matz
         }
      }
 programmer_hash[:yukihiro_matsumoto] = {:known_for =>"ruby", :languages => ["LISP", "C"]}
+programmer_hash
     
 end
+adding_matz
 
 def changing_alan
 	programmer_hash = 
