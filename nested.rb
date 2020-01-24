a2a74a7baa918d167f6fa29cfa15ef6fa63a92c8
Yuksalbek Rakhimjonov <yuksal97@gmail.com>
2020-01-24 06:44:48 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,8 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
+
 
 end
 
@@ -37,6 +39,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  language = programmer_hash[:dennis_ritchie][:languages][0]
+  
 end
 
 def adding_matz
@@ -63,6 +68,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
+
 
 	programmer_hash = 
  		{
@@ -79,9 +85,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto] = {:known_for =>"ruby", :languages => ["LISP", "C"]}
+programmer_hash
     
 end
+adding_matz
 
 def changing_alan
 	programmer_hash = 
@@ -101,9 +109,11 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
-     
+     programmer_hash
 end
+changing_alan
 
 def adding_to_dennis
 	programmer_hash = 
@@ -121,5 +131,10 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+programmer_hash
 
 end
+adding_to_dennis
+
+
