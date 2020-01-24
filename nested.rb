a12ea196766a813cbf80eb8ec1fc0f674ec8c882
Yuksalbek Rakhimjonov <yuksal97@gmail.com>
2020-01-24 06:40:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -111,8 +111,9 @@ def changing_alan
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
      
-     
+     programmer_hash
 end
+changing_alan
 
 def adding_to_dennis
 	programmer_hash = 
